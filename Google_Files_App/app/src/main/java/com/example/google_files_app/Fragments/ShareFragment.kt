ad1e615ab017c0a960837ee64bb3e97fbe2d46eb
ROHITHRATHNA2020 <rohithrathna916@gmail.com>
2021-08-26 23:17:40 +0530

    share fragment

--- Google_Files_App/app/src/main/java/com/example/google_files_app/Fragments/ShareFragment.kt
+++ Google_Files_App/app/src/main/java/com/example/google_files_app/Fragments/ShareFragment.kt
@@ -1,34 +1,23 @@
 package com.example.google_files_app.Fragments
 
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import com.example.google_files_app.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ShareFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ShareFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+
+    val FINE_LOCATION_REQ = 101
+    val WIFI_RQ = 102
+    val BLUETOOTH_REQ = 103
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,23 +27,21 @@ class ShareFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_share, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ShareFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ShareFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
     }
+//
+//    private fun checkForPermission(permission:String, name : String , requestCode: Int){
+//        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+//            when{
+//                context?.let { ContextCompat.checkSelfPermission(it,permission) } == PackageManager. PERMISSION_GRANTED -> {
+//                    Toast.makeText(ContextCompat.checkSelfPermission(permission), "$name permission granted", Toast.LENGTH_SHORT).show()
+//                }
+//                shouldShowRequestPermissionRationale(permission) ->
+//
+//            }
+//        }
+//
+//    }
 }
--- Google_Files_App/app/src/main/java/com/example/google_files_app/MainActivity.kt
+++ Google_Files_App/app/src/main/java/com/example/google_files_app/MainActivity.kt
@@ -41,4 +41,3 @@ class MainActivity : AppCompatActivity() {
         }
 }
 
-//rohith pushed
