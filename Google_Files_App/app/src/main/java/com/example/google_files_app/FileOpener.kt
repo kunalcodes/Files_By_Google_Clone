0475ff0ffcdfcce396f0c2f4cf59c9ce80e27747
Ramsay2 <sachin114sharma@gmail.com>
2021-08-28 00:29:06 +0530

    Categories items opening

--- Google_Files_App/app/src/main/java/com/example/google_files_app/FileOpener.kt
+++ Google_Files_App/app/src/main/java/com/example/google_files_app/FileOpener.kt
@@ -4,11 +4,10 @@ import android.content.Context
 import android.content.Intent
 import androidx.core.content.FileProvider
 import java.io.File
-import java.io.IOException
 
 class FileOpener {
-//    @Throws(IOException::class)
-     companion object {
+    //    @Throws(IOException::class)
+    companion object {
         fun openFile(context: Context, file: File) {
             val selectedFile = file
             val uri = FileProvider.getUriForFile(
@@ -18,13 +17,31 @@ class FileOpener {
             val intent = Intent(Intent.ACTION_VIEW)
             if (uri.toString().contains(".doc")) {
                 intent.setDataAndType(uri, "application/msword")
+
             } else if (uri.toString().contains(".pdf")) {
                 intent.setDataAndType(uri, "application/pdf")
-            } else if (uri.toString().contains(".mp3")) {
+
+            } else if (uri.toString().contains(".mp3")
+                || uri.toString().contains(".m4a")
+                || uri.toString().contains(".wav")
+                || uri.toString().contains(".wma")
+            ) {
                 intent.setDataAndType(uri, "audio/x-wav")
-            } else if (uri.toString().contains(".jpeg")) {
+
+            } else if (uri.toString().contains(".jpeg")
+                || uri.toString().contains(".jpg")
+                || uri.toString().contains(".png")
+                || uri.toString().contains(".bmp")
+            ) {
                 intent.setDataAndType(uri, "image/jpeg")
-            } else if (uri.toString().contains(".mp4")) {
+
+
+            } else if (uri.toString().contains(".mp4")
+                || uri.toString().contains(".mkv")
+                || uri.toString().contains(".flv")
+                || uri.toString().contains(".avi")
+                || uri.toString().contains(".gif")
+            ) {
                 intent.setDataAndType(uri, "video/*")
             } else {
                 intent.setDataAndType(uri, "*/*")
