b5653455adee60851646223d515a038961b1f82c
kunalcodes <kunal.chaturvedi75@gmail.com>
2021-08-29 12:26:56 +0530

    FileOpener.kt operation for .doc file fixed.

--- Google_Files_App/app/src/main/java/com/example/google_files_app/FileOpener.kt
+++ Google_Files_App/app/src/main/java/com/example/google_files_app/FileOpener.kt
@@ -2,6 +2,8 @@ package com.example.google_files_app
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
+import android.webkit.MimeTypeMap
 import androidx.core.content.FileProvider
 import java.io.File
 
@@ -14,7 +16,8 @@ class FileOpener {
                 context.applicationContext.packageName + ".provider", file)
             val intent = Intent(Intent.ACTION_VIEW)
             if (uri.toString().contains(".doc")) {
-                intent.setDataAndType(uri, "application/msword")
+
+                intent.setDataAndType(uri, "text/*")
 
             } else if (uri.toString().contains(".pdf")) {
                 intent.setDataAndType(uri, "application/pdf")
